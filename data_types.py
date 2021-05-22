0ad58ddb6490cd3d6cca8e380083bbcdc58e9cc7
Stefano Taillefert <stefano.taille@gmail.com>
2021-05-22 15:24:55 +0200

    Add missing file lol

---
+++ data_types.py
@@ -0,0 +1,48 @@
+from z3 import Bool
+
+# Garments
+pants = Bool('pants')
+shirt = Bool('shirt')
+hat = Bool('hat')
+jacket = Bool('jacket')
+sweater = Bool('sweater')
+gloves = Bool('gloves')
+shoes = Bool('shoes')
+tie = Bool('tie')
+scarf = Bool('scarf')
+shorts = Bool('shorts')
+
+# Colors
+red = Bool('red')
+yellow = Bool('yellow')
+orange = Bool('orange')
+green = Bool('green')
+blue = Bool('blue')
+purple = Bool('purple')
+brown = Bool('brown')
+pink = Bool('pink')
+white = Bool('white')
+black = Bool('black')
+
+dict = {
+    'pants': pants,
+    'shirt': shirt,
+    'hat': hat,
+    'jacket': jacket,
+    'sweater': sweater,
+    'gloves': gloves,
+    'shoes': shoes,
+    'tie': tie,
+    'scarf': scarf,
+    'shorts': shorts,
+    'red': red,
+    'yellow': yellow,
+    'orange': orange,
+    'green': green,
+    'blue': blue,
+    'purple': purple,
+    'brown': brown,
+    'pink': pink,
+    'white': white,
+    'black': black
+}
