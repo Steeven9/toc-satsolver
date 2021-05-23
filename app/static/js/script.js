e972f29e155eb455d92126dafbba143d5f3ced96
Ted Le <tedle1997@yahoo.com>
2021-05-23 22:27:29 +0200

    fix man behind clothes

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -3,6 +3,7 @@ let nClrs = 0;
 let colors = ['red', 'yellow', ' orange', 'green', 'blue', 'purple', 'brown', 'pink', 'white', 'black'];
 let garments = ['pants', 'shirt', ' hat', 'jacket', 'sweater', 'gloves', 'shoes', 'tie', 'scarf', 'shorts', 'ak'];
 
+
 // DOM targets def
 let addPair = undefined;
 let form = document.getElementById('buildPairs');
@@ -43,8 +44,6 @@ let createForm = () => {
     addPair.innerHTML = 'Add another item';
     addPair.addEventListener('click', handleAddPair);
     form.appendChild(addPair);
-    draw_man();
-
 }
 
 // functionevents
@@ -52,38 +51,10 @@ window.onload = () => {
     if(form){
         createForm();
     }
-
 }
 
 let handleAddPair = (event) => {
     event.preventDefault();
     addSelectPair();
     nClrs++;
-}
-
-let draw_man = (id = "man_canvas", data) =>{
-    var c = document.getElementById(id);
-    var ctx = c.getContext("2d");
-    let offset_y = 100;
-    ctx.beginPath();
-    ctx.arc(200, 50 + offset_y, 40, 0, 2 * Math.PI);
-    ctx.stroke()
-    ctx.moveTo(100, 150+ offset_y)
-    ctx.lineTo(300, 150+ offset_y)
-    ctx.stroke()
-    ctx.moveTo(200, 90+ offset_y)
-    ctx.lineTo(200, 290+ offset_y)
-    ctx.stroke()
-
-    ctx.moveTo(170, 290+ offset_y)
-    ctx.lineTo(230, 290+ offset_y)
-    ctx.stroke()
-
-    ctx.moveTo(170, 290+ offset_y)
-    ctx.lineTo(170, 450+ offset_y)
-    ctx.stroke()
-    ctx.moveTo(230, 290+ offset_y)
-    ctx.lineTo(230, 450+ offset_y)
-    ctx.stroke()
-
 }
